e9e79a9bd700a0e501db375ae371fd3b692cd6b1
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-04 14:44:04 +0700

    menambahkan app.get untuk Menampilkan semua data

--- seeders/index.js
+++ seeders/index.js
@@ -27,3 +27,12 @@ app.post('/kandang', async (req, res) => {
         res.status(500).send({ message: error.message });
     }
 });
+
+app.get('/kandang', async (req, res) => {
+    try {
+        const kandang = await db.kandang.findAll();
+        res.send(kandang);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
