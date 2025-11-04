670a279ae78a94dc4660736112db05cf8594da9c
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-04 14:41:56 +0700

    membuat index.js

---
+++ models/kandang.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => { 
+    const kandang = sequelize.define('kandang', { 
+        ID: {
+            type: DataTypes.INTEGER, 
+            primaryKey: true, 
+            autoIncrement: true  
+        },
+        Nama_Hewan: {
+            type: DataTypes.STRING, 
+            allowNull: false 
+        },
+        Nama_Petugas: {
+            type: DataTypes.STRING, 
+            allowNull: false 
+        },
+        Usia_Hewan: {
+            type: DataTypes.INTEGER, 
+            allowNull: false 
+        },
+        Jenis_hewan: {
+            type: DataTypes.TEXT, 
+            allowNull: false 
+        },
+        Tahun_Lahir: {
+            type: DataTypes.DATE, 
+            allowNull: false 
+        }
+
+    }, {
+        tableName : 'kandang', 
+        timestamps: true, 
+        freezeTableName: true 
+    });
+
+    return kandang; 
+};
---
+++ seeders/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
